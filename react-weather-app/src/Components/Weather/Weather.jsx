cdfcb6c56c912e831bc582e84a2adbfa9d149777
izaak svec <slavi.svec@gmail.com>
2020-09-15 14:33:57 +1000

    update styles

--- react-weather-app/src/Components/Weather/Weather.jsx
+++ react-weather-app/src/Components/Weather/Weather.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import './styles.scss'
 
 const Weather = () => (
   <>
-    <text>Mon</text>
-    <img src='http://openweathermap.org/img/wn/04d@2x.png'></img>
-    <text>19<span>&#176;</span></text>
-    <text>30<span>&#176;</span></text>
+    <section className="weather">
+      <div className="weather__container">
+        <text className="weather__Day">Mon</text>
+        <img className="weather__Image" src='http://openweathermap.org/img/wn/04d@2x.png' alt=''></img>
+        <div className="weather__contantainer-temp">
+          <text className="weather__Temp-min">19<span>&#176;</span></text>
+          <text className="weather__Temp-max">30<span>&#176;</span></text>
+        </div>
+      </div>
+    </section>
   </>
 )
 
