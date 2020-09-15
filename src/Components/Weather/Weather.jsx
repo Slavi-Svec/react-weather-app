e6d6aefcc5b00a613108eed715f27f52685d65e6
izaak svec <slavi.svec@gmail.com>
2020-09-16 08:30:27 +1000

    correct weather component

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -1,24 +1,17 @@
 import React from 'react'
 import './styles.scss'
+import Text from '../Text/Text'
+import Image from '../Image/Image'
 
 const Weather = () => (
-  <>
-    <section className="weather">
-      <div className="weather__container">
-        <text className="weather__Day">Mon</text>
-        <img className="weather__Image" src='http://openweathermap.org/img/wn/04d@2x.png' alt=''></img>
-        <div className="weather__contantainer-temp">
-          <text className="weather__Temp-min">19<span>&#176;</span></text>
-          <text className="weather__Temp-max">30<span>&#176;</span></text>
-        </div>
-      </div>
-    </section>
-  </>
+  <section className="weather">
+    <Text className="weather__day" text="monday"></Text>
+    <Image className="weather__image" src="http://openweathermap.org/img/wn/04d@2x.png"></Image>
+    <div className="weather__container-temp">
+      <Text className="weather__temp-min" showUniCode text="19"></Text>
+      <Text className="weather__temp-max" showUniCode text="38"></Text>
+    </div>
+  </section>
 )
 
 export default Weather
-
-
-
-
-
